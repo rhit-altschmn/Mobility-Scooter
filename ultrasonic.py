75c040a96d102310e0e87f7f0da5ca43fca3923e
rhit-altschmn <95511236+rhit-altschmn@users.noreply.github.com>
2025-11-18 16:53:01 -0800

    Add files via upload

---
+++ ultrasonic.py
@@ -0,0 +1,37 @@
+import serial
+import time
+# Assigned Pins:
+	# S1: Blue(TX) to GPIO14, Green(RX) to GPIO15
+	# S2: TX to GPIO4, RX to GPIO5
+	# S3: TX to GPIO8, RX to GPIO9
+	# S4: TX to GPIO12, RX to GPIO13
+# Setting serial ports for each sensors, skip AMA1
+ports=[
+	serial.Serial("/dev/ttyAMA0",9600,timeout=0.1),
+	serial.Serial("/dev/ttyAMA3",9600,timeout=0.1),
+	serial.Serial("/dev/ttyAMA4",9600,timeout=0.1),
+	serial.Serial("/dev/ttyAMA5",9600,timeout=0.1)
+]
+# Flushing ports and waiting for sensors to stabilize
+for ser in ports:
+	ser.reset_input_buffer()
+time.sleep(1.0)
+
+def read_dist(ser):
+	data=ser.read(4)
+	distance = (data[1] * 256 + data[2])
+	return distance
+
+def cpu_speed():
+	with open("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq") as cpu:
+		return int(cpu.read().strip()) / 1000 # Convert to mHz
+
+while True:
+	distances = [] 
+	for i, ser in enumerate(ports): # Add each distance reading
+		dist = read_dist(ser)
+		distances.append(f"S{i+1}: {dist} mm")
+	speed = cpu_speed()
+	distances.append(f"CPU Speed: {speed}")
+	print(" ".join(distances))
+	time.sleep(0.05)
