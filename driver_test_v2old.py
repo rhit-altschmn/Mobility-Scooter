a9e63e09ccb462eb227363c73c6ca20fe46f329d
rhit-altschmn <95511236+rhit-altschmn@users.noreply.github.com>
2025-12-03 11:18:22 -0800

    front-back test code

--- driver_test_v2old.py
+++ driver_test_v2old.py
@@ -7,23 +7,24 @@ GPIO.setup(38,GPIO.OUT) # relay 1 hi/lo
 GPIO.setup(37,GPIO.OUT) # DAC2 pwm
 GPIO.setup(35,GPIO.OUT) # relay 2 hi/lo
 
-pwm = GPIO.PWM(40,50) #set duty cycle to 50Hz or 20 ms
-pwm2 = GPIO.PWM(37,50) #set duty cycle to 50Hz or 20 ms
+pwm = GPIO.PWM(40,1000) #set duty cycle to 50Hz or 20 ms
+pwm2 = GPIO.PWM(37,1000) #set duty cycle to 50Hz or 20 ms
 #operating frequency is 20Hz-22khz
-x = 10 #command input
+x = -1 #command input #1 for forward, -1 for backward
+for i in range(1,20,1):
+	pwm.start(50+20*x) #sets pwm to 7.5% of duty cycle, or 1.5ms
+	pwm2.start(50-20*x) #sets pwm to 7.5% of duty cycle, or 1.5ms
+	sleep(.25)
 
-pwm.start(50+x) #sets pwm to 7.5% of duty cycle, or 1.5ms
-pwm2.start(50-x) #sets pwm to 7.5% of duty cycle, or 1.5ms
-sleep(1)
+	GPIO.output(38, 0)
+	GPIO.output(35, 0)
+	sleep(1)
 
-GPIO.output(38, 1)
-GPIO.output(35, 1)
-sleep(10)
-
-pwm.ChangeDutyCycle(50)
-pwm2.ChangeDutyCycle(50)
-GPIO.output(38, 0)
-GPIO.output(35, 0)
+	pwm.ChangeDutyCycle(50)
+	pwm2.ChangeDutyCycle(50)
+	GPIO.output(38, 1)
+	GPIO.output(35, 1)
+	sleep(.5)
 
 #pwm.stop()
 
